89e86854c8f8b6538d13415ae40e237776d81c2a
Your_Void <my.vo1d.edge@gmail.com>
2023-12-23 17:16:27 +0300

    Добавление комментариев

--- XSS_scaner.py
+++ XSS_scaner.py
@@ -6,12 +6,25 @@ from auth_module import AuthModule
 
 # Функция получения всех форм страницы
 def get_all_forms(session, url):
+    """
+    Получает все формы на странице.
+
+    :param session: Сеанс сессии requests.
+    :param url: URL страницы.
+    :return: Список объектов форм.
+    """
     soup = bs(session.get(url).content, "html.parser")
     return soup.find_all("form")
 
 
 # Функция получения всех деталей форм и атрибутов
 def get_form_details(form):
+    """
+    Получает детали формы, такие как атрибуты и поля ввода.
+
+    :param form: Объект формы BeautifulSoup.
+    :return: Словарь с деталями формы.
+    """
     details = {}
     action = form.attrs.get("action", "").lower()
     method = form.attrs.get("method", "get").lower()
@@ -28,6 +41,15 @@ def get_form_details(form):
 
 # Функция для отправки формы
 def submit_form(session, form_details, url, values):
+    """
+    Отправляет форму с указанными значениями.
+
+    :param session: Сеанс сессии requests.
+    :param form_details: Детали формы.
+    :param url: URL страницы.
+    :param values: Значения для полей формы.
+    :return: Ответ после отправки формы.
+    """
     target_url = urljoin(url, form_details["action"])
     inputs = form_details["inputs"]
     data = {}
@@ -41,7 +63,13 @@ def submit_form(session, form_details, url, values):
         return session.get(target_url, params=data)
 
 def scan_xss(session, url):
-    # Продолжение сканирования на уязвимость XSS после успешной аутентификации
+    """
+    Сканирует страницу на уязвимость XSS после успешной аутентификации.
+
+    :param session: Сеанс сессии requests с выполненной аутентификацией.
+    :param url: URL страницы.
+    :return: True, если уязвимость обнаружена, иначе False.
+    """
     forms = get_all_forms(session, url)
     js_script = "<script>alert('hi')</script>"
     is_vulnerable = False
@@ -53,9 +81,13 @@ def scan_xss(session, url):
         form_values = {}
         for input in form_details["inputs"]:
             if input["type"] == "text" or input["type"] == "search":
+                # Используем предостережение от XSS в качестве значения
                 form_values[input["name"]] = js_script
 
+        # Отправляем форму с предостережением от XSS
         content = submit_form(session, form_details, url, form_values).content.decode()
+
+        # Проверяем, содержится ли предостережение от XSS в полученном контенте
         if js_script in content:
             is_vulnerable = True
             break
